82beb3ff7f590bd919d65caeb8a2771f8c2ce0b4
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 22:51:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
 def roll_call_dwarves(dwarves)
   hash = Hash.new
   dwarves.each_with_index(1) do |value, index|
-    puts "#{index}: #{value}"
-  
+    hash[value] = index
+    puts "#{hash}"
+
   end
 end
 
