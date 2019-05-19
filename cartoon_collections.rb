e05e50b0fdf281b219bb3c9b2118bc284c8fe278
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 23:15:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.include? do |check|
-    check
+  calls.any? do |check|
+    check.size
 end
 
 def find_the_cheese# code an argument here
