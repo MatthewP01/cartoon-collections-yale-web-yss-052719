6d28ae925d2c5bd6c28ab75495c69bd0c650c941
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 22:59:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls
+  planeteer_calls.split
   planeteer_calls.collect do |alter|
     alter.capitalize
     alter
