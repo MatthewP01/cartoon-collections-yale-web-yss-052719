6f277e8ea2f01d8b17c77891692c936985b881a8
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 22:49:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index(1) do |value, index|
     puts "#{index}: #{value}"
-  # Your code here
+  end
 end
 
 def summon_captain_planet# code an argument here
