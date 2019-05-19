e419765c0763dd05ae876d1d2630648cb5b505f7
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 23:12:36 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |alter|
     alter.capitalize + "!"
   end
-  return planeteer_calls
+  # return planeteer_calls
   # Your code here
 end
 
